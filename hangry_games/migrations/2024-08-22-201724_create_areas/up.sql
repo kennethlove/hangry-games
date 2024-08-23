42df51d1de00de0bc0504dff7f0505e563c63c4d
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-08-23 12:46:33 -0700

    Added migration to add default Areas

--- hangry_games/migrations/2024-08-22-201724_create_areas/up.sql
+++ hangry_games/migrations/2024-08-22-201724_create_areas/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS areas (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL UNIQUE
 )
---
+++ hangry_games/migrations/2024-08-23-194307_add-areas/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO areas (name) VALUES ('The Cornucopia');
+INSERT INTO areas (name) VALUES ('Northwest');
+INSERT INTO areas (name) VALUES ('Northeast');
+INSERT INTO areas (name) VALUES ('Southeast');
+INSERT INTO areas (name) VALUES ('Southwest');
