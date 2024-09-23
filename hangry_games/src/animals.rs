11716001f7ce46c54a99a6620121923fab324063
Kenneth Love <kennethlove@gmail.com>
2024-09-23 02:04:30 +0000

    Lowercase animal names

--- hangry_games/src/animals.rs
+++ hangry_games/src/animals.rs
@@ -24,27 +24,27 @@ pub enum Animal {
 impl Animal {
     pub fn as_str(&self) -> &str {
         match self {
-            Animal::Squirrel => "Squirrel",
-            Animal::Bear => "Bear",
-            Animal::Wolf => "Wolf",
-            Animal::Cougar => "Cougar",
-            Animal::Boar => "Boar",
-            Animal::Snake => "Snake",
-            Animal::Monkey => "Monkey",
-            Animal::Baboon => "Baboon",
-            Animal::Hyena => "Hyena",
-            Animal::Lion => "Lion",
-            Animal::Tiger => "Tiger",
-            Animal::Elephant => "Elephant",
-            Animal::Rhino => "Rhino",
-            Animal::Hippo => "Hippo",
-            Animal::TrackerJacker => "Tracker Jacker",
+            Animal::Squirrel => "squirrel",
+            Animal::Bear => "bear",
+            Animal::Wolf => "wolf",
+            Animal::Cougar => "cougar",
+            Animal::Boar => "boar",
+            Animal::Snake => "snake",
+            Animal::Monkey => "monkey",
+            Animal::Baboon => "baboon",
+            Animal::Hyena => "hyena",
+            Animal::Lion => "lion",
+            Animal::Tiger => "tiger",
+            Animal::Elephant => "elephant",
+            Animal::Rhino => "rhino",
+            Animal::Hippo => "hippo",
+            Animal::TrackerJacker => "tracker jacker",
         }
     }
 
     pub fn plural(&self) -> String {
         match self {
-            Animal::Wolf => "Wolves".to_string(),
+            Animal::Wolf => "wolves".to_string(),
             _ => {
                 let pluralized = format!("{}s", self.as_str());
                 pluralized
@@ -124,21 +124,21 @@ impl FromStr for Animal {
 impl Display for Animal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Animal::Squirrel => write!(f, "Squirrel"),
-            Animal::Bear => write!(f, "Bear"),
-            Animal::Wolf => write!(f, "Wolf"),
-            Animal::Cougar => write!(f, "Cougar"),
-            Animal::Boar => write!(f, "Boar"),
-            Animal::Snake => write!(f, "Snake"),
-            Animal::Monkey => write!(f, "Monkey"),
-            Animal::Baboon => write!(f, "Baboon"),
-            Animal::Hyena => write!(f, "Hyena"),
-            Animal::Lion => write!(f, "Lion"),
-            Animal::Tiger => write!(f, "Tiger"),
-            Animal::Elephant => write!(f, "Elephant"),
-            Animal::Rhino => write!(f, "Rhino"),
-            Animal::Hippo => write!(f, "Hippo"),
-            Animal::TrackerJacker => write!(f, "Tracker Jacker"),
+            Animal::Squirrel => write!(f, "squirrel"),
+            Animal::Bear => write!(f, "bear"),
+            Animal::Wolf => write!(f, "wolf"),
+            Animal::Cougar => write!(f, "cougar"),
+            Animal::Boar => write!(f, "boar"),
+            Animal::Snake => write!(f, "snake"),
+            Animal::Monkey => write!(f, "monkey"),
+            Animal::Baboon => write!(f, "baboon"),
+            Animal::Hyena => write!(f, "hyena"),
+            Animal::Lion => write!(f, "lion"),
+            Animal::Tiger => write!(f, "tiger"),
+            Animal::Elephant => write!(f, "elephant"),
+            Animal::Rhino => write!(f, "rhino"),
+            Animal::Hippo => write!(f, "hippo"),
+            Animal::TrackerJacker => write!(f, "tracker jacker"),
         }
     }
 }
