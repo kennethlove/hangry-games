6fd88047a31d63a8f706ace03984c290cbff78e1
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-10-07 11:53:51 -0700

    Rename migration, add new action type

--- hangry_games/migrations/2024-08-25-154102_create-and-add-actions/up.sql
+++ hangry_games/migrations/2024-08-25-154102_create-and-add-actions/up.sql
@@ -9,6 +9,7 @@ CREATE TABLE action (
 INSERT INTO action (name, description) VALUES ('none', 'Do nothing');
 INSERT INTO action (name, description) VALUES ('move', 'Move to a new location');
 INSERT INTO action (name, description) VALUES ('rest', 'Rest and recover');
-INSERT INTO action (name, description) VALUES ('use-item', 'Use an item');
+INSERT INTO action (name, description) VALUES ('use item', 'Use an item');
 INSERT INTO action (name, description) VALUES ('attack', 'Attack another tribute');
 INSERT INTO action (name, description) VALUES ('hide', 'Hide to avoid detection');
+INSERT INTO action (name, description) VALUES ('take item', 'Take an item');
