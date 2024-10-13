9c357f682431ac7037220f7d2ac892c3616409c5
Kenneth Love <kennethlove@gmail.com>
2024-10-12 18:05:38 -0700

    UI supports adding a new game. List shows new game and input clears.

--- hangry_games/src/bin/hangry.rs
+++ hangry_games/src/bin/hangry.rs
@@ -22,13 +22,81 @@ fn Header() -> Element {
 }
 
 #[component]
-fn Animal() -> Element {
-    let mut animal = use_signal(||new_animal());
-
+fn Home() -> Element {
     rsx! {
+        p { "Welcome to the Hangry Games!" }
+        div {
+            class: "grid grid-cols-2",
+            GameList {}
+        }
         div {
             class: "grid grid-cols-2",
             CreateGame {}
+        }
+    }
+}
+
+#[component]
+fn GameList() -> Element {
+    let games = use_signal(||list_of_games());
+
+    rsx! {
+        div {
+            h2 { "Games" }
+            ul {
+                for game in games.read().iter() {
+                    GameListItem { game: game.clone() }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn GameListItem(game: Game) -> Element {
+    let mut selected_game = use_context::<Signal<SelectedGame>>();
+    rsx! {
+        li {
+            Link {
+                onclick: move |_| { selected_game.set(SelectedGame(Some(game.id.unwrap()))) },
+                to: Route::GameDetail {},
+                "{game.name}, {game.living_tributes().len()}/24 tributes"
+            }
+        }
+    }
+}
+
+#[component]
+fn GameDetail() -> Element {
+    let selected_game = use_context::<Signal<SelectedGame>>();
+    let game = Game::from(get_game_by_id(selected_game.read().0.unwrap()).unwrap());
+    rsx! {
+        div {
+            h2 { "Game Detail" }
+            h3 { "{game.name} on day {game.day.unwrap_or(0)}" }
+            h4 { "Tributes" }
+            ul {
+                for tribute in game.living_tributes() {
+                    li { "{tribute.name}" }
+                }
+            }
+        }
+        Link {
+            to: Route::Home {},
+            "Home"
+        }
+    }
+}
+
+#[component]
+fn CreateGame() -> Element {
+    rsx! {
+        div {
+            h2 { "Create Game" }
+            form {
+                input { r#type: "text", placeholder: "Game Name" }
+                button { "Create Game" }
+            }
         }
     }
 }
