242f54fb1eabfa7d3a5922ff27ef0ad3d50aa87f
Kenneth Love <kennethlove@gmail.com>
2024-10-13 12:02:13 -0700

    Lots.
    * Attempted to get assets working, no luck
    * Added ability to delete games
    * Improved game list appearance
    * Improved game detail appearance
    * Game creation takes you to game detail

--- hangry_games/src/bin/hangry.rs
+++ hangry_games/src/bin/hangry.rs
@@ -17,7 +17,9 @@ fn list_of_games() -> Vec<Game> {
 #[component]
 fn Header() -> Element {
     rsx! {
-        h1 { "Hangry Games" }
+        h1 {
+            "Hangry Games"
+        }
     }
 }
 
@@ -38,14 +40,34 @@ fn Home() -> Element {
 
 #[component]
 fn GameList() -> Element {
-    let games = use_signal(||list_of_games());
+    let mut state = use_context::<Signal<HGState>>();
 
     rsx! {
         div {
             h2 { "Games" }
-            ul {
-                for game in games.read().iter() {
-                    GameListItem { game: game.clone() }
+            table {
+                class: "min-w-full divide-y-2 divide-gray-200 bg-white text-sm",
+                thead {
+                    class: "ltr:text-left rtl:text-right",
+                    tr {
+                        th {
+                            class: "whitespace-nowrap px-4 py-2 font-medium text-gray-900",
+                            "Name"
+                        }
+                        th {
+                            class: "whitespace-nowrap px-4 py-2 font-medium text-gray-900",
+                            "Day"
+                        }
+                        th {
+                            class: "px-4 py-2"
+                        }
+                    }
+                }
+                tbody {
+                    class: "divide-y divide-gray-200",
+                    for game in state.read().games.iter() {
+                        GameListItem { game: game.clone() }
+                    }
                 }
             }
         }
@@ -56,12 +78,80 @@ fn GameList() -> Element {
 fn GameListItem(game: Game) -> Element {
     let mut selected_game = use_context::<Signal<SelectedGame>>();
     rsx! {
-        li {
-            Link {
-                onclick: move |_| { selected_game.set(SelectedGame(Some(game.id.unwrap()))) },
-                to: Route::GameDetail {},
-                "{game.name}, {game.living_tributes().len()}/24 tributes"
+        tr {
+            td {
+                class: "whitespace-nowrap px-4 py-2 text-gray-700",
+                Link {
+                    onclick: move |_| { selected_game.set(SelectedGame(Some(game.id.unwrap()))) },
+                    to: Route::GameDetail {},
+                    "{game.name}, {game.living_tributes().len()}/24 tributes"
+                }
             }
+            td {
+                "{game.day.unwrap_or(0)}"
+            }
+            td {
+                class: "flex-end",
+                GameActionsGroup { game: game.clone() }
+            }
+        }
+    }
+}
+
+#[component]
+fn GameActionsGroup(game: Game) -> Element {
+    rsx! {
+        div {
+            class: "inline-flex rounded-lg border border-gray-100 bg-gray-100 p-1",
+            GameDeleteButton { game: game.clone() }
+            GameDetailsButton { game: game.clone() }
+            GamePlayButton { game: game.clone() }
+        }
+    }
+}
+
+#[component]
+fn GameDeleteButton(game: Game) -> Element {
+    let mut state = use_context::<Signal<HGState>>();
+    rsx! {
+        button {
+            class: "inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-red-700 focus:relative",
+            onclick: move |_| {
+                Game::delete(game.id.unwrap());
+                state.write().games.retain(|g| g.id != game.id);
+            },
+            "Delete"
+        }
+    }
+}
+
+#[component]
+fn GameDetailsButton(game: Game) -> Element {
+    let mut state = use_context::<Signal<HGState>>();
+    let nav = navigator();
+
+    rsx! {
+        button {
+            class: "inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-blue-700 focus:relative",
+            onclick: move |_| {
+                let mut selected_game = use_context::<Signal<SelectedGame>>();
+                selected_game.set(SelectedGame(Some(game.id.unwrap())));
+                nav.push(Route::GameDetail {});
+            },
+            "Details"
+        }
+    }
+}
+
+#[component]
+fn GamePlayButton(game: Game) -> Element {
+    let mut state = use_context::<Signal<HGState>>();
+    rsx! {
+        button {
+            class: "inline-block rounded-md px-4 py-2 text-sm text-gray-500 hover:text-green-700 focus:relative",
+            onclick: move |_| {
+            },
+            "Play"
         }
     }
 }
@@ -72,15 +162,53 @@ fn GameDetail() -> Element {
     let game = Game::from(get_game_by_id(selected_game.read().0.unwrap()).unwrap());
     rsx! {
         div {
-            h2 { "Game Detail" }
-            h3 { "{game.name} on day {game.day.unwrap_or(0)}" }
-            h4 { "Tributes" }
-            ul {
-                for tribute in game.living_tributes() {
-                    li { "{tribute.name}" }
+            class: "flow-root",
+            dl {
+                class: "-my-3 divide-y divide-gray-100 text-sm",
+                div {
+                    class: "grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4",
+                    dt {
+                        class: "font-medium text-gray-900",
+                        "Name"
+                    }
+                    dd {
+                        class: "text-gray-700 sm:col-span-2",
+                        "{game.name}"
+                    }
+                }
+                div {
+                    class: "grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4",
+                    dt {
+                        class: "font-medium text-gray-900",
+                        "Day"
+                    }
+                    dd {
+                        class: "text-gray-700 sm:col-span-2",
+                        "{game.day.unwrap_or(0)}"
+                    }
+                }
+                div {
+                    class: "grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4",
+                    dt {
+                        class: "font-medium text-gray-900",
+                        "Living Tributes"
+                    }
+                    dd {
+                        class: "text-gray-700 sm:col-span-2",
+                        ul {
+                            class: "divide-y divide-gray-200",
+                            for tribute in game.living_tributes() {
+                                li {
+                                    class: "flex items-center py-3",
+                                    "{tribute.name}"
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+
         Link {
             to: Route::Home {},
             "Home"
@@ -90,15 +218,45 @@ fn GameDetail() -> Element {
 
 #[component]
 fn CreateGame() -> Element {
+    let mut state = use_context::<Signal<HGState>>();
+    let mut game_name = use_signal(String::new);
+    let nav = navigator();
+
     rsx! {
         div {
             h2 { "Create Game" }
             form {
-                input { r#type: "text", placeholder: "Game Name" }
+                onsubmit: move |event| {
+                    let data = event.data.values();
+                    let game_name = data.get("game_name").unwrap().first().unwrap();
+                    let game = Game::new(game_name);
+                    let mut selected_game = use_context::<Signal<SelectedGame>>();
+                    selected_game.set(SelectedGame(Some(game.id.unwrap())));
+                    state.write().games.push(game);
+                    nav.push(Route::GameDetail {});
+                },
+                input {
+                    r#type: "text",
+                    placeholder: "Game Name",
+                    id: "game_name",
+                    name: "game_name",
+                    value: "{game_name}",
+                    oninput: move |event| game_name.set(event.value().clone()),
+                    onkeypress: move |event| {
+                        if event.key() == Key::Enter {
+                            game_name.set(String::from(""))
+                        }
+                    }
+                }
                 button { "Create Game" }
             }
         }
     }
+}
+
+#[derive(Debug)]
+struct HGState {
+    games: Vec<Game>,
 }
 
 fn app() -> Element {
