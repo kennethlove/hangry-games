3366d19c354ab825d8b3090a9ae824a2075c622f
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-10-14 11:50:19 -0700

    Tributes can now be deleted

--- hangry_games/src/gui/components/home.rs
+++ hangry_games/src/gui/components/home.rs
@@ -1,15 +1,18 @@
 use dioxus::prelude::*;
+use crate::games::Game;
 use crate::gui::components::{
     game_table::GameTable,
     create_game::CreateGame,
 };
+use crate::gui::functions::list_of_games;
 
 #[component]
 pub fn Home() -> Element {
+    let games_signal: Signal<Vec<Game>> = use_signal(||list_of_games());
     rsx! {
         p { "Welcome to the Hangry Games!" }
         div {
-            GameTable {}
+            GameTable { games: games_signal.clone() }
         }
         div {
             CreateGame {}
