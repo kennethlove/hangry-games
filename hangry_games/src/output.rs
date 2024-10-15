847a3de575a2d2e835d016294b8e1ea5caed0643
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-10-15 14:23:32 -0700

    Improves LogEntry, and moves some println content to constants.

---
+++ hangry_games/src/output.rs
@@ -0,0 +1,10 @@
+// Collection on strings to be used as output for the game
+
+// Game day starts
+pub const GAME_DAY_START: &str = "=== ☀️ Day {} begins! ===";
+pub const FIRST_DAY_START: &str = "=== 🎉 The Hunger Games begin! 🎉 ===";
+pub const FEAST_DAY_START: &str = "=== 😋 Day 3: Feast Day ===";
+
+// End conditions
+pub const NO_ONE_WINS: &str = "=== 🎭 No one wins! ===";
+pub const TRIBUTE_WINS: &str = "=== 🏆 The winner is {} ===";
