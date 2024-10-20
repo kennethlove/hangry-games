435f81fa07bd875971214afee93f0ed15953fe73
Kenneth Love <kennethlove@gmail.com>
2024-10-20 11:32:30 -0700

    Nicer tribute boxes, added link back to home

--- hangry_games/src/gui/components/tribute_actions_group.rs
+++ hangry_games/src/gui/components/tribute_actions_group.rs
@@ -7,7 +7,7 @@ use crate::tributes::actors::Tribute;
 pub fn TributeActionsGroup(tribute: Tribute, signal: Signal<Vec<Tribute>>) -> Element {
     rsx! {
         div {
-            class: "inline-flex rounded-lg bg-gradient-to-r from-orange-500 to-yellow-300 p-1",
+            class: "inline-flex justify-between w-full rounded-lg bg-gradient-to-r from-orange-500 to-yellow-300 p-1 divide-x divide-gray-300",
             TributeDeleteButton { tribute: tribute.clone(), signal: signal }
             TributeDetailsButton { tribute: tribute.clone() }
         }
@@ -18,7 +18,7 @@ pub fn TributeActionsGroup(tribute: Tribute, signal: Signal<Vec<Tribute>>) -> El
 fn TributeDeleteButton(tribute: Tribute, signal: Signal<Vec<Tribute>>) -> Element {
     rsx! {
         button {
-            class: "inline-block rounded-md px-4 py-2 text-sm font-normal text-slate-800 hover:text-red-700 focus:relative",
+            class: "inline-block w-full px-4 py-2 text-sm font-normal text-slate-800 hover:text-red-700 focus:relative",
             onclick: move |_| {
                 Tribute::delete(tribute.id.unwrap());
                 signal.write().retain(|t| t.id != tribute.id);
@@ -38,7 +38,7 @@ fn TributeDetailsButton(tribute: Tribute) -> Element {
 
     rsx! {
         button {
-            class: "inline-block rounded-md px-4 py-2 text-sm font-normal text-slate-800 hover:text-blue-700 focus:relative",
+            class: "inline-block w-full px-4 py-2 text-sm font-normal text-slate-800 hover:text-blue-700 focus:relative",
             onclick: move |_| {
                 nav.push(Routes::TributeDetail { id: tribute.id.unwrap() });
             },
--- hangry_games/src/gui/components/tribute_box.rs
+++ hangry_games/src/gui/components/tribute_box.rs
@@ -6,26 +6,37 @@ use crate::tributes::actors::Tribute;
 #[component]
 pub fn TributeBox(tribute: Tribute, signal: Signal<Vec<Tribute>>) -> Element {
     rsx! {
-        div {
-            class: "rounded-lg bg-gradient-to-r from-orange-500 to-yellow-300 p-4",
+        a {
+            class: "group relative block bg-black",
+            img {
+                class: "absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50",
+                src: "https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+            }
+
             div {
-                class: "flex justify-between",
-                h2 {
-                    class: "text-2xl font-bold text-slate-900",
-                    "{tribute.name}"
-                },
+                class: "relative p-4 sm:p-6 lg:p-8",
                 p {
-                    class: "text-lg text-slate-700",
-                    "{tribute.district}"
-                },
+                    class: "text-sm font-medium uppercase tracking-widest text-orange-500",
+                    "District {tribute.district}"
+                }
+                p {
+                    class: "text-xl font-bold text-white sm:text-2xl",
+                    "{tribute.name}"
+                }
+                div {
+                    class: "mt-32 sm:mt-48 lg:mt-64",
+                    div {
+                        class: "translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100",
+                        p {
+                            class: "w-full text-center text-sm text-slate-400 mb-4 font-normal",
+                            span { class: "material-symbols-outlined text-sm", "favorite" }, " {tribute.status} ",
+                            span { class: "material-symbols-outlined text-sm", "vital_signs" }, " {tribute.health} ",
+                            span { class: "material-symbols-outlined text-sm", "cognition_2" }, " {tribute.sanity} ",
+                        }
+                        TributeActionsGroup { tribute: tribute.clone(), signal: signal.clone() }
+                    }
+                }
             }
-            p {
-                class: "text-sm text-slate-700 mb-4 font-semibold",
-                span { class: "material-symbols-outlined text-sm", "favorite" }, " {tribute.status} ",
-                span { class: "material-symbols-outlined text-sm", "vital_signs" }, " {tribute.health} ",
-                span { class: "material-symbols-outlined text-sm", "cognition_2" }, " {tribute.sanity} ",
-            }
-            TributeActionsGroup { tribute: tribute.clone(), signal: signal.clone() }
         }
     }
 }
