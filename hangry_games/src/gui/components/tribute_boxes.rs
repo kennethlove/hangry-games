4ecdafd4d917a56bc67d97aa774a3c50a08fdc6e
Kenneth Love <kennethlove@gmail.com>
2024-10-20 13:30:34 -0700

    Always show a multiple of 2 columns

--- hangry_games/src/gui/components/tribute_boxes.rs
+++ hangry_games/src/gui/components/tribute_boxes.rs
@@ -7,7 +7,7 @@ use crate::tributes::actors::Tribute;
 pub fn TributeBoxes(tributes: Signal<Vec<Tribute>>) -> Element {
     rsx! {
         div {
-            class: "grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",
+            class: "grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6",
             for tribute in tributes.read().iter() {
                 TributeBox { tribute: tribute.clone(), signal: tributes.clone() }
             }
