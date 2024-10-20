03a313b2dd1049db0555e04d7ab9314c679e929e
Kenneth Love <kennethlove@gmail.com>
2024-10-20 11:06:36 -0700

    Better game detail information layout

--- hangry_games/src/gui/components/game_detail.rs
+++ hangry_games/src/gui/components/game_detail.rs
@@ -14,34 +14,16 @@ pub fn GameDetail(id: i32) -> Element {
 
     rsx! {
         div {
-            class: "flow-root",
-            dl {
-                class: "-my-3 divide-y divide-gray-100 text-sm",
-                div {
-                    class: "grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4",
-                    dt {
-                        class: "font-medium text-gray-900",
-                        "Name"
-                    }
-                    dd {
-                        class: "text-gray-700 sm:col-span-2",
-                        "{game.name}"
-                    }
-                }
-                div {
-                    class: "grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4",
-                    dt {
-                        class: "font-medium text-gray-900",
-                        "Day"
-                    }
-                    dd {
-                        class: "text-gray-700 sm:col-span-2",
-                        "{game.day.unwrap_or(0)}"
-                    }
-                }
+            class: "flex justify-between items-center",
+            h2 {
+                class: "text-2xl font-bold text-slate-900",
+                "{game.name}"
+            }
+            h3 {
+                class: "text-lg text-slate-700",
+                "Day {game.day.unwrap_or(0)}"
             }
         }
-
         div {
             class: "mt-4",
             TributeBoxes { tributes: tributes.clone() }
