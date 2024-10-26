f2d22c95f11476b5fd27cd7e37831d7f5c1e06c0
Kenneth Love <kennethlove@gmail.com>
2024-10-26 11:32:21 -0700

    Cleaner approach

--- hangry_games/src/bin/hangry.rs
+++ hangry_games/src/bin/hangry.rs
@@ -6,16 +6,6 @@ use hangry_games::gui::router::Routes;
 use hangry_games::gui::states::HGState;
 use hangry_games::gui::functions::list_of_games;
 
-#[cfg(target_family = "windows")]
-fn custom_head() -> String {
-    r#"<link rel="stylesheet" href="assets/hangry-games.css">"#.to_string()
-}
-
-#[cfg(target_family = "unix")]
-fn custom_head() -> String {
-    r#"<link rel="stylesheet" href="hangry-games.css">"#.to_string()
-}
-
 fn main() {
     dioxus_logger::init(Level::INFO).expect("logger failed to init");
     let mut head = r#"<script src="https://cdn.tailwindcss.com"></script>
@@ -23,7 +13,12 @@ fn main() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />"#.to_string();
-    head.push_str(&custom_head());
+
+    if cfg!(target_family = "windows") {
+        head.push_str(r#"<link rel="stylesheet" href="assets/hangry-games.css">"#);
+    } else {
+        head.push_str(r#"<link rel="stylesheet" href="hangry-games.css">"#);
+    }
 
     let config = Config::new()
         .with_custom_head(head)
